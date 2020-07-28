9a8a6a93f68649ec99469f72e36d54bf3ca1ddf2
yang-julian <juliany@google.com>
2020-07-27 20:05:17 -0700

    Initial test setup, including unit test and integration test.
    Note that integration tests must conform to the following naming
    patterns:
      "**/IT*.java"
      "**/*IT.java"
      "**/*ITCase.java"

---
+++ src/test/java/com/google/sps/servlets/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.google.sps.servlets;
+
+// @RunWith(DevAppServerTestRunner.class)
+// @DevAppServerTest(HomePageTest.TestConfig.class)
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleUnitTest {
+  @Test
+  public void testAThing() throws Exception {
+    // Just a silly example.
+    int a = 1;
+    int b = 2;
+    Assert.assertFalse(a == b);
+  }
+}
