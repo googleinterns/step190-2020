7bdb981ea7c766c649e70c873abc868901e1db1f
Casey Price <caseyprice217@gmail.com>
2020-07-29 16:39:20 -0700

    Attempt #2: Add headless flag

--- src/test/java/com/google/sps/integration/ElectionListIT.java
+++ src/test/java/com/google/sps/integration/ElectionListIT.java
@@ -25,6 +25,7 @@ public class ElectionListIT {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
+    options.addArguments('--headless');
     options.setExperimentalOption("useAutomationExtension", false);
     driver = new ChromeDriver(options);
   }
