08046c2661f6f7ca0a1e244a6523b2d944374f90
Casey Price <caseyprice217@gmail.com>
2020-07-29 10:26:33 -0700

    Apply AutoValue Builder plugin to Candidate, PollingStation, Position, and Proposition objects

--- src/main/java/com/google/sps/data/Position.java
+++ src/main/java/com/google/sps/data/Position.java
@@ -1,18 +1,47 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps.data;
 
+import com.google.auto.value.AutoValue;
 import java.util.HashMap;
 
-/** A public office position that may be open for election on voter ballots */
-public final class Position {
-  private final long id;
-  private final String name;
-  private final HashMap<Long, Candidate> candidates;
-  private final String description;
+/** A state or national public office position. */
+@AutoValue
+public abstract class Position {
+  public abstract long getID();
 
-  public Position(PositionBuilder builder) {
-    this.id = builder.getID();
-    this.name = builder.getName();
-    this.candidates = builder.getCandidates();
-    this.description = builder.getDescription();
+  public abstract String getName();
+
+  public abstract HashMap<Long, Candidate> getCandidates();
+
+  public abstract String getDescription();
+
+  public static Builder builder() {
+    return new AutoValue_Position.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setID(long id);
+
+    public abstract Builder setName(String name);
+
+    public abstract Builder setCandidates(HashMap<Long, Candidate> candidates);
+
+    public abstract Builder setDescription(String description);
+
+    public abstract Position build();
   }
 }
