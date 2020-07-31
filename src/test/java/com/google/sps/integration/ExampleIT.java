d498c319d96dc143c82d8827a248e162ca66a969
yang-julian <juliany@google.com>
2020-07-31 15:49:37 -0700

    Try copying local_db.bin for integration test data seeding

--- src/test/java/com/google/sps/integration/ExampleIT.java
+++ src/test/java/com/google/sps/integration/ExampleIT.java
@@ -1,21 +1,70 @@
 package com.google.sps.integration;
 
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.appengine.tools.development.testing.LocalURLFetchServiceTestConfig;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
-import org.junit.Test;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.*;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 // Note: Integration tests must conform to the following naming pattern:
 //   "**/IT*.java"
 //   "**/*IT.java"
 //   "**/*ITCase.java"
 public class ExampleIT {
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(
+          new LocalDatastoreServiceTestConfig(), new LocalURLFetchServiceTestConfig());
+
+  private WebDriver driver;
+  @Mock HttpServletRequest httpServletRequest;
+  @Mock HttpServletResponse httpServletResponse;
+  @Mock PrintWriter printWriter;
+
+  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  @ClassRule
+  public static ChromeDriverPropertySetup chromeDriverPropertySetup =
+      new ChromeDriverPropertySetup();
+
+  @Before
+  public void testSetUp() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--headless");
+    options.setExperimentalOption("useAutomationExtension", false);
+    driver = new ChromeDriver(options);
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+    driver.quit();
+  }
 
   @Test
   public void testEndToEnd() throws Exception {
     // We should probably pass in the port number into the test, but IMO it's more trouble than it's
     // worth right now.
-    URL url = new URL("http://localhost:9876");
+    String response = getUrlResponse("http://localhost:9876/election");
+    System.out.println(response);
+    Thread.sleep(1000 * 100);
+  }
+
+  private String getUrlResponse(String urlAddress) throws Exception {
+    URL url = new URL(urlAddress);
     StringBuilder response = new StringBuilder();
     try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
       String inputLine;
@@ -23,6 +72,6 @@ public class ExampleIT {
         response.append(inputLine).append('\n');
       }
     }
-    System.out.println(response.toString());
+    return response.toString();
   }
 }
