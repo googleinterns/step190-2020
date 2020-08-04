bf40e44d80ab7746cbceaf568a60acc7437cf109
anooshrees <alsy2k@gmail.com>
2020-08-04 23:14:01 +0000

    Additional IT

---
+++ src/test/java/com/google/sps/integration/ElectionInfoIT.java
@@ -0,0 +1,37 @@
+package com.google.sps.integration;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+public class ElectionListIT {
+  private WebDriver driver;
+
+  @ClassRule
+  public static ChromeDriverPropertySetup chromeDriverPropertySetup =
+      new ChromeDriverPropertySetup();
+
+  @Before
+  public void testSetUp() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--headless");
+    options.setExperimentalOption("useAutomationExtension", false);
+    driver = new ChromeDriver(options);
+  }
+
+  /**
+   * TODO(anooshree): Resolve issue of being unable to test address input box locally due to 
+   *                  API restrictions
+   */
+
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+}
