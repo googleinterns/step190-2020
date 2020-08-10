73c01e78d9d7dc4e1bb627d524d0c53b0a054518
anooshrees <anooshree@google.com>
2020-08-10 19:11:08 +0000

    Initial support for polling station map.

--- src/main/webapp/election-info.js
+++ src/main/webapp/election-info.js
@@ -1,3 +1,15 @@
+// The config file contains the unrestricted key for use on localhost
+var mapKey = (typeof config === 'undefined' || config === null) 
+                ? 'AIzaSyCBrkGl891qgFHNFz3VGi3N-CEXGLp-DIU' : config.PLACES_API_KEY;
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://maps.googleapis.com/maps/api/js?key='
+             + mapKey + '&callback=initializeMap&libraries=&v=weekly';
+script.defer = true;
+
+document.head.appendChild(script);
+
 function onElectionInfoLoad(){
   let searchParams = new URLSearchParams(window.location.search);
   let selectedElection = searchParams.get("electionName"); 
@@ -25,7 +37,6 @@ function logAddressInput(){
 
   callInfoCardServlet();
   populateClassesForTemplate();
-  // TODO(anooshree): create map
 }
 
 /**
@@ -74,5 +85,59 @@ function populateClassesForTemplate(){
 
       let infoCardContainerElement = document.getElementById('contests-referendums-content');
       infoCardContainerElement.innerHTML = template(context);
+  });
+}
+
+$(document).ready(function(){
+    $('#address-input-js').on('click',initializeMap)
+});
+
+var geocoder;
+function initializeMap() {
+  geocoder = new google.maps.Geocoder();
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const address = urlParams.get('address');
+  
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.3382, lng: -121.8863 },
+    zoom: 8
+  });
+
+  fetch('/polling-stations')
+    .then(response => response.json())
+    .then((pollingStationList) => {
+      pollingStationList.forEach((pollingStation) => {
+        // addPollingStation call
+      });
+    });
+}
+
+/**
+ * Adds a polling station as a marker on the map, with an info card
+ * that displays upon click to show more information about the station
+ */
+function addPollingStation(address, map, title, description) {
+  geocoder.geocode({
+    componentRestrictions: {country: 'US'},
+    'address': address}, function(results, status) {
+      if (status == 'OK') {
+        addLandmark(map, results[0].geometry.location, title, description);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+  });
+}
+
+/** 
+ * Adds a marker that shows an info window when clicked. 
+ */
+function addLandmark(map, position, title, description) {
+  const marker = new google.maps.Marker(
+      {position: position, map: map, title: title});
+
+  const infoWindow = new google.maps.InfoWindow({content: description});
+  marker.addListener('click', () => {
+    infoWindow.open(map, marker);
   });
 }
