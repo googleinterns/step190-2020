d6c53ce40256a40ab8932a51f1b0bfc4a5b5d7c3
Casey Price <caseyprice@google.com>
2020-08-11 14:29:07 -0700

    Create collapsible description sections for the info cards (#93)

--- src/main/webapp/election-info.js
+++ src/main/webapp/election-info.js
@@ -6,6 +6,20 @@ function onElectionInfoLoad(){
     const chosenElection = document.getElementById('chosen-election');
     chosenElection.innerText = selectedElection;
   }
+
+  let collapsibles = document.getElementsByClassName("collapsible");
+
+  for (let i = 0; i < collapsibles.length; i++) {
+    collapsibles[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + 36 + "px";
+      }
+    });
+  }
 }
 
 /**
