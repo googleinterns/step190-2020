46e70902e094e548223c4e50a2f33742c7b009cb
anooshree <anooshree@google.com>
2020-08-13 11:54:02 -0700

    Integration test for autocomplete address input

--- src/test/java/com/google/sps/integration/ElectionInfoIT.java
+++ src/test/java/com/google/sps/integration/ElectionInfoIT.java
@@ -6,10 +6,13 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ElectionInfoIT {
   private WebDriver driver;
@@ -42,7 +45,30 @@ public class ElectionInfoIT {
         "http://localhost:9876/electionInfo.html?state=ca&electionId=2000", driver.getCurrentUrl());
   }
 
-  // TODO(anooshree): Check that Query URL updates after address submission
+  @Test
+  public void addressSubmission_onClick_createAndUpdateQueryParams() throws InterruptedException {
+
+    driver.get("http://localhost:9876/electionInfo.html?state=ca&electionId=2000");
+    WebDriverWait wait = new WebDriverWait(driver, 20);
+
+    WebElement autocompleteBox = driver.findElement(By.id("autocomplete"));
+    autocompleteBox.sendKeys("121 Bernal Road, San Jose");
+    Thread.sleep(5000);
+
+    wait.until(ExpectedConditions.visibilityOfElementLocated((By.id("autocomplete"))))
+        .sendKeys(Keys.DOWN, Keys.ENTER);
+
+    Thread.sleep(1000);
+
+    WebElement submitButton = driver.findElement(By.id("submit-address-button"));
+    submitButton.click();
+
+    String targetAddressValue = "121+Bernal+Road+San+Jose+CA+95119+United+States";
+    Assert.assertEquals(
+        "http://localhost:9876/electionInfo.html?state=ca&electionId=2000&address="
+            + targetAddressValue,
+        driver.getCurrentUrl());
+  }
 
   // TODO(anooshree): Check that content loads for a valid URL
 
