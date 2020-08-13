8d29b1aa589c12fb14d2fe70b353084df5574a5c
Casey Price <caseyprice@google.com>
2020-08-13 15:52:53 -0700

    Add tests to check for error screen and fix other integration test election names

--- src/test/java/com/google/sps/integration/ElectionInfoIT.java
+++ src/test/java/com/google/sps/integration/ElectionInfoIT.java
@@ -1,9 +1,13 @@
 package com.google.sps.integration;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -22,6 +26,37 @@ public class ElectionInfoIT {
     options.addArguments("--headless");
     options.setExperimentalOption("useAutomationExtension", false);
     driver = new ChromeDriver(options);
+  }
+
+  /**
+   * Tests if the error screen shows when the URL does not contain an election ID query parameter.
+   */
+  @Test
+  public void noElectionIdInQueryUrl_displayErrorText() {
+    driver.get("http://localhost:9876/electionInfo.html");
+    WebElement contentElement = driver.findElement(By.id("election-name-template"));
+    WebElement paragraphElement = contentElement.findElement(By.xpath("//p[@id='title-text']"));
+    Assert.assertEquals("Error! No election selected.", paragraphElement.getText());
+
+    WebElement wrapperElement = driver.findElement(By.id("election-info-wrapper"));
+    Assert.assertEquals("none", wrapperElement.getCssValue("display"));
+  }
+
+  /**
+   * Tests if the error screen shows when the URL does not contain an election ID query parameter.
+   */
+  @Test
+  public void electionIdInQueryUrl_displayRegularInfoCardScreen() {
+    driver.get("http://localhost:9876/electionInfo.html?state=ca&electionId=2000");
+    WebElement contentElement = driver.findElement(By.id("election-name-template"));
+    WebElement paragraphElement = contentElement.findElement(By.xpath("//p[@id='title-text']/b"));
+    Assert.assertEquals(
+        "To show you polling locations and ballot items from the VIP Test Election, "
+            + "we\'ll need your registered voter address:",
+        paragraphElement.getText());
+
+    WebElement wrapperElement = driver.findElement(By.id("election-info-wrapper"));
+    Assert.assertEquals("block", wrapperElement.getCssValue("display"));
   }
 
   /**
