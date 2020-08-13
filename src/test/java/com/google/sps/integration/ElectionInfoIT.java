71a61af618855ab78a5d74314f15941cb86cdc51
anooshree <anooshree@google.com>
2020-08-12 22:02:24 -0700

    update integration.db again

--- src/test/java/com/google/sps/integration/ElectionInfoIT.java
+++ src/test/java/com/google/sps/integration/ElectionInfoIT.java
@@ -6,13 +6,10 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ElectionInfoIT {
   private WebDriver driver;
