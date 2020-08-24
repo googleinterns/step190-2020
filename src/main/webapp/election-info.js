9b5dd18ff6d6f46055b57d8d2ed9119f86ed470c
Casey Price <caseyprice@google.com>
2020-08-24 16:59:24 -0700

    Remove individual address input fields

--- src/main/webapp/election-info.js
+++ src/main/webapp/election-info.js
@@ -47,12 +47,7 @@ function onElectionInfoLoad(){
  * Enable the submit button if this element contains text.
  */
 setInterval(function() {
-  if(document.getElementById('street_number').value == '' 
-      || document.getElementById('route').value == ''
-      || document.getElementById('locality').value == ''
-      || document.getElementById('administrative_area_level_1').value == ''
-      || document.getElementById('country').value == ''
-      || document.getElementById('postal_code').value == '') { 
+  if(document.getElementById('autocomplete').value == '' ) {
     document.getElementById('submit-address-button').disabled = true;
   } else { 
     document.getElementById('submit-address-button').disabled = false;
@@ -64,15 +59,9 @@ setInterval(function() {
  * query URL and call functions to populate the info cards
  */
 function logAddressInput() {
-  let streetNumber = document.getElementById('street_number').value;
-  let route = document.getElementById('route').value;
-  let city = document.getElementById('locality').value;
-  let state = document.getElementById('administrative_area_level_1').value;
-  let zipCode = document.getElementById('postal_code').value;
-  let country = document.getElementById('country').value;
+  let address = document.getElementById('autocomplete').value;
 
-  addQueryParameter("address", 
-                    `${streetNumber} ${route} ${city} ${state} ${zipCode} ${country}`);
+  addQueryParameter("address", address);
 
   let searchParams = new URLSearchParams(window.location.search);
   
@@ -111,8 +100,6 @@ function callInfoCardServlet(electionId, address){
       } else {
         errorTextElement.style.display = "block";
         document.getElementById('polling-stations-map').style.height = '0';
-        document.getElementById('dates-and-deadlines').innerHTML = '';
-        document.getElementById('dates-and-deadlines').style.display = 'none';
         document.getElementById('no-polling-stations').innerHTML = '';
         document.getElementById('election-info-results').innerHTML = '';
         hideSpinner();
@@ -157,8 +144,6 @@ function populateDeadlines(state) {
       deadlinesContainerElement.innerHTML = template(context);
       deadlinesContainerElement.style.display = 'block';
       console.log("processed deadlines");
-
-      
   });
 }
 
@@ -201,31 +186,23 @@ function populateClassesForTemplate(electionId){
       let infoCardContainerElement = document.getElementById('election-info-results');
       infoCardContainerElement.innerHTML = template(context);
 
-      document.getElementById('autocomplete').value = '' ;
-      document.getElementById('street_number').value = '' ;
-      document.getElementById('route').value = '';
-      document.getElementById('locality').value = '';
-      document.getElementById('administrative_area_level_1').value = '';
-      document.getElementById('country').value = '';
-      document.getElementById('postal_code').value = '';
-
       let collapsibles = document.getElementsByClassName("collapsible");
 
       for (let i = 0; i < collapsibles.length; i++) {
         collapsibles[i].addEventListener("click", function() {
-          this.classList.toggle("active");
           let content = this.nextElementSibling;
           if (content.style.height && content.style.height !== '0px'){
             content.style.height = '0px';
           } else {
             content.style.height = '100%'; 
           }
+          this.classList.toggle("active");
         });
       }
 
       hideSpinner();
       window.scrollTo({
-        top: window.innerHeight - 40,
+        top: window.innerHeight - 80,
         left: 0,
         behavior: 'smooth'
       });
@@ -352,13 +329,13 @@ function addPollingStationMarker(map, position, title, description) {
 }
 
 /**
- * Handlebars helper that removes the beginning "http://", "https://" and the trailing "/"
+ * Handlebars helper that removes the beginning "http://", "https://", any "www.", and the trailing "/"
  * from the supplied URL string.
  * 
  * @param {String} URL to be stripped.
  */
 Handlebars.registerHelper('stripUrl', function(urlString){
-  urlString = urlString.replace(/(^\w+:|^)\/\/(www.)?/, '');
+  urlString = urlString.replace(/(^\w+:|^)\/\/(www\.)?/, '');
   if(urlString[urlString.length - 1] == '/'){
     urlString = urlString.slice(0, -1);
   }
